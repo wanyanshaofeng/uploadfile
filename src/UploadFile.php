205222d7dc2cd39ec1fc2fc5ad0806e1f06908df
wanyanshaofeng <wanyanshaofeng@163.com>
2019-03-21 15:58:07 +0800

    init

---
+++ src/UploadFile.php
@@ -0,0 +1,8 @@
+<?php
+namespace Wanyan;
+
+class UploadFile{
+	public function add(){  
+		echo '这是一个测试包';
+	}
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+require dirname(__FILE__).'/vendor/autoload.php';
+use Wanyan\UploadFile;
+
+$uf = new UploadFile();
+$uf -> add();
